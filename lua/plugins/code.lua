775d8a3599dbba657a868c421ef6723932f8745f
coolliuzw <1413883407@qq.com>
2024-04-11 22:59:34 +0800

    add go refa

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -51,4 +51,17 @@ return {
       })
     end,
   },
+  {
+    "edolphin-ydf/goimpl.nvim",
+    enabled = true,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    config = function()
+      require("telescope").load_extension("goimpl")
+    end,
+  },
 }
