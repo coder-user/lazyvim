2bb28ea01c63ca21834254e056c82e247f153d1f
coolliuzw <1413883407@qq.com>
2024-04-16 12:27:51 +0800

    some code

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -8,9 +8,6 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        -- "gitui",
-        "goimports",
-        "gofumpt",
         "staticcheck",
         "golangci-lint",
       })
---
+++ lua/plugins/go.lua
@@ -0,0 +1,29 @@
+if false then
+  return {}
+end
+
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "goimports_reviser" })
+        end,
+      },
+    },
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.code_actions.gomodifytags,
+        nls.builtins.code_actions.impl,
+        nls.builtins.formatting.goimports_reviser, -- 导入顺序的支持
+        -- nls.builtins.formatting.goimports,
+        -- nls.builtins.formatting.gofumpt,
+      })
+    end,
+  },
+}
