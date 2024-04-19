6d2b7e8d466f0145e632984170ae8363c5dafe4f
coolliuzw <1413883407@qq.com>
2024-04-19 21:33:30 +0800

    some code

---
+++ lua/config/me.lua
@@ -0,0 +1,15 @@
+if false then
+  return {}
+end
+
+-- LSP keymaps
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+    end,
+  },
+}
